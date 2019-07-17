120731828282665d0fde0178a9118aab62af0a69
Stefan Ziegler <edi.gonzales@gmail.com>
2019-07-17 21:01:34 +0200

    initial import

---
+++ src/main/java/ch/so/agi/oereb/wicketclient/OerebWicketClientApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.oereb.wicketclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OerebWicketClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OerebWicketClientApplication.class, args);
+	}
+
+}
