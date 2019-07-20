6e8988c78740e31d32712c21f62fffa601a892d3
Stefan Ziegler <edi.gonzales@gmail.com>
2019-07-20 19:07:42 +0200

    wip

--- src/main/java/ch/so/agi/oereb/wicketclient/wicket/HomePage.java
+++ src/main/java/ch/so/agi/oereb/wicketclient/wicket/HomePage.java
@@ -1,29 +1,54 @@
 package ch.so.agi.oereb.wicketclient.wicket;
 
+import java.util.Date;
+
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 
 
 @WicketHomePage
 public class HomePage extends WebPage {
-    
+    private String egrid = "CH158782774974";
+
     public HomePage() {
-        Form form = new Form("form"){
+        Form<HomePage> form = new Form<HomePage>("form", new CompoundPropertyModel<HomePage>(this)) {
             @Override
             protected void onSubmit() {
-                setResponsePage(SecondPage.class);
+                //setResponsePage(SecondPage.class);
+                System.out.println("Form submitted.");
+                System.out.println(egrid);
             }
         };
-        queue(form);
+        add(form);
         
-        add(new Link<Void>("mylink") {
+        TextField<String> egridField = new TextField<String>("egrid"); 
+        form.add(egridField);
+        
+        
+//        add(new Link<Void>("mylink") {
+//            @Override
+//            public void onClick() {
+//                setResponsePage(SecondPage.class);
+//            }
+//        });
+        
+        IModel timeStampModel = new Model<String>(){
             @Override
-            public void onClick() {
-                setResponsePage(SecondPage.class);
+            public String getObject() {
+                return new Date().toString();
             }
-        });
+        };
+        //add(new Label("timeStamp", timeStampModel));
+        add(new Label("timeStamp", () -> java.time.LocalDateTime.now()));
+
+        
     }
 }
