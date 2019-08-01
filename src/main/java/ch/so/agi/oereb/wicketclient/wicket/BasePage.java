50a495dd65e1db3d5cdb60c7db39fc8e4d33b745
Stefan Ziegler <edi.gonzales@gmail.com>
2019-08-01 18:54:35 +0200

    start w/ basepage (but i guess i will not use it

---
+++ src/main/java/ch/so/agi/oereb/wicketclient/wicket/BasePage.java
@@ -0,0 +1,10 @@
+package ch.so.agi.oereb.wicketclient.wicket;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class BasePage extends WebPage {
+    
+    public BasePage() {
+        
+    }
+}
